a3b311937e0ab88b0f6544972e11b8f98a60d894
Khoi Hoang <khoang@cs.nmsu.edu>
2017-10-21 16:07:39 -0500

    First commit

---
+++ dcop_gen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#python dcop_gen_rand.py -a 50 -d 10 -p 0.5 -r 2 -c 100 -n random -o instances/random
+for i in {0..50}
+  python dcop_gen_rand_py -a $1 -d $2 -p $3 -r $4 -c $5 -n random -o instances/

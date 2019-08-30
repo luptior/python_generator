83c875f96d2ab9015f9b4fe45ca16db2e86d78d5
Gan Xu <luptior@gmail.com>
2019-08-30 05:41:13 -0500

    update

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 output=data
 
-n_agents=10
-n_dom=10
+n_agents=5
+n_dom=100
 repo_size=10
 p1=0.8
 p2=0.6
@@ -10,7 +10,7 @@ max_cost=100
 repo_size=10
 
 for rep in $(seq 1 $repo_size); do
-  for n_dom in 50 60; do
+  for n_dom in 100 120 140 160 180 200; do
     name=$output/"a"${n_agents}"_d"${n_dom}"_r"$rep
     python src/dcop_gen_rand.py --agts $n_agents \
                             --doms $n_dom \

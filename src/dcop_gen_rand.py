373d1b1c7528abf1ce76882afc84480426422d38
Gan Xu <gan.xu@wustl.edu>
2020-09-30 13:45:19 -0500

    Update dcop_gen_rand.py

--- src/dcop_gen_rand.py
+++ src/dcop_gen_rand.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
 
     print('Creating DCOP instance ' + name)
     dcop.create_xml_instance(name, agts, vars, doms, cons, outfile + '.xml')
-    dcop.create_wcsp_instance(name, agts, vars, doms, cons, outfile + '.wcsp')
-    dcop.create_json_instance(name, agts, vars, doms, cons, outfile + '.json')
-    dcop.create_maxsum_instance(name, agts, vars, doms, cons, outfile + '.maxsum')
-    dcop.create_dalo_instance(name, agts, vars, doms, cons, outfile + '.dalo')
+    # dcop.create_wcsp_instance(name, agts, vars, doms, cons, outfile + '.wcsp')
+    # dcop.create_json_instance(name, agts, vars, doms, cons, outfile + '.json')
+    # dcop.create_maxsum_instance(name, agts, vars, doms, cons, outfile + '.maxsum')
+    # dcop.create_dalo_instance(name, agts, vars, doms, cons, outfile + '.dalo')
